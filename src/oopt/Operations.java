2f60bd245ddb6d263891757b02ba455ec4473b23
LeeJTing <168332482+LeeJTing@users.noreply.github.com>
2024-09-22 13:57:18 +0800

    Update Operations.java

--- src/oopt/Operations.java
+++ src/oopt/Operations.java
@@ -1,1 +1,7 @@
+package oopt;
 
+interface Operations {
+    public abstract void addNewInstance();
+    public abstract void modifyInstanceInformation();
+    public abstract void deleteInstance();
+}
